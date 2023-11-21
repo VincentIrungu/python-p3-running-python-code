6acd1af310ff9a037636ebeb17d498a4916c0b74
VincentIrungu <vrugundu001@gmail.com>
2023-11-21 09:18:40 +0300

    Complete Assignment

---
+++ lib/app.py
@@ -0,0 +1,15 @@
+# lib/app.py
+print("Hello world!", end=" ")
+print("Pass this test," end=", ")
+print("please", end=".\n ")
+
+print("Hello world!")
+#print("Hello sun!")
+#print("Hello sky")
+
+
+# choosing your own print ()Ending
+#print("Hello World!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!\n")
+
